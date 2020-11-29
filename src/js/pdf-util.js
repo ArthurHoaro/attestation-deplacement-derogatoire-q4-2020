2a3373c2c25cd51bc203172f9f12a009b617fb11
ArthurHoaro <arthur@hoa.ro>
2020-11-29 14:04:52 +0100

    Update PDF with latest online version

--- src/js/pdf-util.js
+++ src/js/pdf-util.js
@@ -1,19 +1,20 @@
-import { generateQR } from './util'
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib'
 
+import { generateQR } from './util'
+
 const ys = {
-  travail: 488,
-  achats: 417,
-  sante: 347,
-  famille: 325,
-  handicap: 291,
-  sport_animaux: 269,
-  convocation: 199,
-  missions: 178,
-  enfants: 157,
+  travail: 553,
+  achats_culturel_cultuel: 482,
+  sante: 434,
+  famille: 410,
+  handicap: 373,
+  sport_animaux: 349,
+  convocation: 276,
+  missions: 252,
+  enfants: 228,
 }
 
-export async function generatePdf (profile, reasons, pdfBase) {
+export async function generatePdf(profile, reasons, pdfBase) {
   const creationInstant = new Date()
   const creationDate = creationInstant.toLocaleDateString('fr-FR')
   const creationHour = creationInstant
@@ -70,30 +71,28 @@ export async function generatePdf (profile, reasons, pdfBase) {
     page1.drawText(text, { x, y, size, font })
   }
 
-  drawText(`${firstname} ${lastname}`, 107, 657)
-  drawText(birthday, 107, 627)
-  drawText(placeofbirth, 240, 627)
-  drawText(`${address} ${zipcode} ${city}`, 124, 596)
+  drawText(`${firstname} ${lastname}`, 92, 702)
+  drawText(birthday, 92, 684)
+  drawText(placeofbirth, 214, 684)
+  drawText(`${address} ${zipcode} ${city}`, 104, 665)
 
-  reasons
-    .split(', ')
-    .forEach(reason => {
-      drawText('x', 59, ys[reason], 12)
-    })
+  reasons.split(', ').forEach((reason) => {
+    drawText('x', 47, ys[reason], 12)
+  })
 
   let locationSize = getIdealFontSize(font, profile.city, 83, 7, 11)
 
   if (!locationSize) {
     alert(
       'Le nom de la ville risque de ne pas être affiché correctement en raison de sa longueur. ' +
-        'Essayez d\'utiliser des abréviations ("Saint" en "St." par exemple) quand cela est possible.',
+      'Essayez d\'utiliser des abréviations ("Saint" en "St." par exemple) quand cela est possible.'
     )
     locationSize = 7
   }
 
-  drawText(profile.city, 93, 122, locationSize)
-  drawText(`${profile.datesortie}`, 76, 92, 11)
-  drawText(`${profile.heuresortie}`, 246, 92, 11)
+  drawText(profile.city, 78, 76, locationSize)
+  drawText(`${profile.datesortie}`, 63, 58, 11)
+  drawText(`${profile.heuresortie}`, 227, 58, 11)
 
   // const shortCreationDate = `${creationDate.split('/')[0]}/${
   //   creationDate.split('/')[1]
@@ -111,7 +110,14 @@ export async function generatePdf (profile, reasons, pdfBase) {
 
   const qrImage = await pdfDoc.embedPng(generatedQR)
 
-  page1.drawText(qrTitle1 + '\n' + qrTitle2, { x: 415, y: 135, size: 9, font, lineHeight: 10, color: rgb(1, 1, 1) })
+  page1.drawText(`${qrTitle1}\n${qrTitle2}`, {
+    x: 440,
+    y: 130,
+    size: 6,
+    font,
+    lineHeight: 10,
+    color: rgb(1, 1, 1),
+  })
 
   page1.drawImage(qrImage, {
     x: page1.getWidth() - 156,
@@ -122,7 +128,13 @@ export async function generatePdf (profile, reasons, pdfBase) {
 
   pdfDoc.addPage()
   const page2 = pdfDoc.getPages()[1]
-  page2.drawText(qrTitle1 + qrTitle2, { x: 50, y: page2.getHeight() - 70, size: 11, font, color: rgb(1, 1, 1) })
+  page2.drawText(qrTitle1 + qrTitle2, {
+    x: 50,
+    y: page2.getHeight() - 70,
+    size: 11,
+    font,
+    color: rgb(1, 1, 1),
+  })
   page2.drawImage(qrImage, {
     x: 50,
     y: page2.getHeight() - 390,
@@ -135,7 +147,7 @@ export async function generatePdf (profile, reasons, pdfBase) {
   return new Blob([pdfBytes], { type: 'application/pdf' })
 }
 
-function getIdealFontSize (font, text, maxWidth, minSize, defaultSize) {
+function getIdealFontSize(font, text, maxWidth, minSize, defaultSize) {
   let currentSize = defaultSize
   let textWidth = font.widthOfTextAtSize(text, defaultSize)
 
